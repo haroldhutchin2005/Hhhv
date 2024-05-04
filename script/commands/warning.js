3463cfc241c389481ae45a0aaa62ed3f9cfd9a19
Harold Hutchins <160975590+haroldhutchin2005@users.noreply.github.com>
2024-05-04 19:50:19 +0800

    Create warning.js

---
+++ script/commands/warning.js
@@ -0,0 +1,40 @@
+module.exports.config = {
+  name: "warninghaha",
+  version: "1.0.0",
+  hasPermssion: 0,
+  credits: "jonell Magallanes",
+  hide: true,
+  description: "otherbot",
+  commandCategory: "...",
+  cooldowns: 0
+};
+
+module.exports.handleEvent = async ({
+	event: o,
+	api: t,
+	Users: n
+}) => {
+	var {
+		threadID: e,
+		messageID: a,
+		body: b,
+		senderID: s,
+		reason: d
+	} = o;
+
+	if (s == t.getCurrentUserID()) return;
+	let c = await n.getNameUser(o.senderID);
+	var h = {
+		body: `ⓘ This comment was deleted, and the account of this user is under investigation for racism, hate crimes, and domestic terrorism.`
+	};
+	// Add curse words without capital letters
+	["niga", "putang", "cum", "fuck", "yawa", "pennis", "gago", "stupid", "nig*", "fuck up", "oten", "Nigas", "bobo", "putang ina mo", "ina mo", "tanga kaba", "shit", "dick"].forEach((word) => {
+		if (o.body.includes(word)) {
+			modules = "[ BOT BAN ]", console.log(c, modules, a);
+			t.sendMessage(h, e);
+		}
+	});
+}, module.exports.run = async ({
+	event: o,
+	api: t
+}) => t.sendMessage("This command is used to detect sensitivity words.", o.threadID);
